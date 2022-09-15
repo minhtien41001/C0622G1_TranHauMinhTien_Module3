561fb34f7cfc6e89614cf922e6b6c5934c2e2421
minhtien41001 <tranminhtien41001@gmail.com>
2022-09-15 19:45:38 +0700

    update

---
+++ bai_3/bai_2.sql
@@ -0,0 +1,40 @@
+use quan_ly_ban_hang;
+
+insert into Customer(cID,cName,cAge)
+values
+(1,"Minh Quan",10),
+(2,"Ngoc Oanh",20);
+
+insert into `order` 
+values (1,1,'2006-3-21',null),
+(2,2,'2006-3-23',null),
+(3,1,'2006-3-16',null);
+
+insert into Product 
+values (1, 'May Giat', 3),
+(2, 'Tu Lanh', 5),
+(3, 'Dieu Hoa', 7),
+(4, 'Quat', 1),
+(5, 'Bep Dien', 2);
+
+insert into OrderDetail 
+values(1, 1, 3),
+(1, 3, 7),
+(1, 4, 2),
+(2, 1, 1), 
+(3, 1, 8),
+(2, 5, 4),
+(2, 3, 3);
+
+select oID, oDate, oTotalPrice from `order`;
+select  Customer.cName, Product.pName
+from Customer
+inner join `order` on Customer.cID = `order`.cID
+inner join OrderDetail  on `order`.oID= OrderDetail.oID
+inner join Product on OrderDetail.pID = Product.pID;
+select *from Customer where Customer.cID not in (select cID from `order`);
+select `order`.oID,`order`.oDate,SUM(OrderDetail.odQTY*Product.pPrice) as oTotalPrice
+from `order`
+inner join OrderDetail on `order`.oID = OrderDetail.oID
+inner join Product on OrderDetail.pID = Product.pID
+group by `order`.oID;
