e9e31b0f2a88f47de8b2370c5fef34c95e2324e1
minhtien41001 <tranminhtien41001@gmail.com>
2022-09-30 20:22:52 +0700

    update

--- ss11/pratice/customer-management/src/main/java/com/codegym/service/CustomerService.java
+++ ss11/pratice/customer-management/src/main/java/com/codegym/service/CustomerService.java
@@ -1,2 +1,17 @@
-package com.codegym.service;public interface CustomerService {
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer>findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id,Customer customer);
+
+    void remove(int id);
 }
